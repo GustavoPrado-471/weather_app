b21923a7940ca5486b1e099806d099e6909e7f29
GustavoPrado-471 <martinezgustavoalberto471@gmail.com>
2023-04-07 19:52:14 -0600

    API si no responde

--- src/app/services/weather.service.ts
+++ src/app/services/weather.service.ts
@@ -23,6 +23,7 @@ export class WeatherService {
       headers: new HttpHeaders()
         .set(this.XRapidAPIHostHeaderName, this.XRapidAPIHostHeaderValue)
         .set(this.XRapidAPIKeyHeaderName, this.XRapidAPIKeyHeaderValue),
+      // params: new HttpParams().set('/', cityName),
     });
   }
 }
